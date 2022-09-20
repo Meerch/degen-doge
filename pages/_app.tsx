44295b9f3d9692ae6890051ea10eafb8ee7818f0
Meerch <khukhriansk333@gmail.com>
2022-09-21 00:04:37 +0300

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,20 @@ import store from '../redux/store'
 import '../styles/globals.scss'
 
 function App({Component, pageProps}: AppProps) {
-  return <>
-    <Head>
-      <title>Degen Doge</title>
-    </Head>
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  </>
+    return <>
+        <Head>
+            <link rel="icon" type="image/ico" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+            <link rel="icon" type="image/ico" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+            <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+            <link rel="manifest" href="/favicon/site.webmanifest.json"/>
+
+            <title>Degen Doge</title>
+        </Head>
+        <Provider store={store}>
+            <Component {...pageProps} />
+        </Provider>
+    </>
 }
 
 const makeStore = () => store
