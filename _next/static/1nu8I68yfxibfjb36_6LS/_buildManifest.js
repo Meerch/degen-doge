27610c8572dc747117d1193167d2f4577bdde863
Meerch <56489774+Meerch@users.noreply.github.com>
2022-09-20 22:28:08 +0000

    Deploying to gh-pages from @ Meerch/degen-doge@15f8e4c9038828e812e6a1eab9ffc513335421ee 🚀

---
+++ _next/static/1nu8I68yfxibfjb36_6LS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f8cdeaa019405a19.css","static/chunks/pages/index-86c471898ec148ab.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ISOx9GUD9KUMfh_kFFZYX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f8cdeaa019405a19.css","static/chunks/pages/index-81493613098c03bf.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
