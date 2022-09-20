04c679bff57b76adc3e9b0f150afac5331df3af8
Meerch <56489774+Meerch@users.noreply.github.com>
2022-09-20 16:06:56 +0000

    Deploying to gh-pages from @ Meerch/degen-doge@33427fa76a9d720da186f8a2e62b61d508e68bb4 🚀

---
+++ _next/static/tJsb2hhWQCzUI1ROgxOju/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a75885d18c63bfeb.css","static/chunks/pages/index-78e9dc21004be262.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
