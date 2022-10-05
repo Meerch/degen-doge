d90915e5c35d716a2505f34698bea3cf28c92d21
Meerch <khukhriansk333@gmail.com>
2022-10-05 13:50:06 +0300

    add marquee for interline

--- components/InterlineLayer/Tape/index.tsx
+++ components/InterlineLayer/Tape/index.tsx
@@ -4,23 +4,28 @@ import styles from './Tape.module.scss'
 const Tape = () => {
     return (
         <div className={styles.tape}>
-            <div className={styles.text}>
-                <span className={styles.word}>MARKETPLACE SOON</span>
-                <span className={styles.word}>5555 NFTs</span>
-                <span className={styles.word}>HOSTED ON IPFS</span>
-                <span className={styles.word}>MARKETPLACE SOON</span>
-                <span className={styles.word}>5555 NFTs</span>
-                <span className={styles.word}>HOSTED ON IPFS</span>
-            </div>
-
-            <div className={styles.text}>
-                <span className={styles.word}>MARKETPLACE SOON</span>
-                <span className={styles.word}>5555 NFTs</span>
-                <span className={styles.word}>HOSTED ON IPFS</span>
-                <span className={styles.word}>MARKETPLACE SOON</span>
-                <span className={styles.word}>5555 NFTs</span>
-                <span className={styles.word}>HOSTED ON IPFS</span>
-            </div>
+            {
+                new Array(2).fill('').map((_, index) => (
+                    <div key={index} className={styles.text}>
+                        <div className={styles.wrapper}>
+                            <span className={styles.word}>MARKETPLACE SOON</span>
+                            <span className={styles.word}>5555 NFTs</span>
+                            <span className={styles.word}>HOSTED ON IPFS</span>
+                            <span className={styles.word}>MARKETPLACE SOON</span>
+                            <span className={styles.word}>5555 NFTs</span>
+                            <span className={styles.word}>HOSTED ON IPFS</span>
+                        </div>
+                        <div className={styles.wrapper}>
+                            <span className={styles.word}>MARKETPLACE SOON</span>
+                            <span className={styles.word}>5555 NFTs</span>
+                            <span className={styles.word}>HOSTED ON IPFS</span>
+                            <span className={styles.word}>MARKETPLACE SOON</span>
+                            <span className={styles.word}>5555 NFTs</span>
+                            <span className={styles.word}>HOSTED ON IPFS</span>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
