9b490e21aea9aeb9634be7347b0d7b44ccdb74ad
Meerch <khukhriansk333@gmail.com>
2022-10-24 08:43:42 +0300

    fix error build

--- components/Popup/PopupBuyNft/index.tsx
+++ components/Popup/PopupBuyNft/index.tsx
@@ -8,69 +8,69 @@ import LoadingButton from "./StatesButton/LoadingButton"
 import {useMint} from "./useMint"
 
 const PopupBuyNft = () => {
-    const [amount, setAmount] = useState(1)
-    const {
-        isViewActiveButton,
-        availableTokensToMint,
-        handlerClickActionButton,
-        checkIsDisableButton,
-        isError,
-        isLoading,
-        isApprove
-    } = useMint(amount)
-
-    const handlerCounter = (value: number) => {
-        const plannedValue = amount + value
-        if (plannedValue > 0 && plannedValue <= +availableTokensToMint) {
-            setAmount(prev => prev + value)
-        }
-    }
-
-    return (
-        <PopupLayout>
-            <div className={styles.popup}>
-                <span className={styles.title}>buy NFT with $DC</span>
-
-                <div className={styles.buttons}>
-                    <div className={styles.counter}>
-                        <span onClick={() => handlerCounter(-1)} className={styles.minus}>-</span>
-                        <span className={styles.value}>{amount}</span>
-                        <span onClick={() => handlerCounter(1)} className={styles.plus}>+</span>
-                    </div>
-
-                    <Button
-                        onClick={handlerClickActionButton}
-                        className={classNames(styles.button, {
-                            [styles.disabled]: checkIsDisableButton(),
-                            [styles.loading]: isLoading,
-                            [styles.error]: isError
-                        })}
-                    >
-                        {
-                            isViewActiveButton &&
-                            <span className={styles.text}>
-                                    {
-                                        isApprove
-                                            ? 'click to buy'
-                                            : 'approve $DC'
-                                    }
-                                </span>
-                        }
-
-                        {
-                            isLoading && <LoadingButton/>
-                        }
-
-                        {
-                            isError && <ErrorButton/>
-                        }
-                    </Button>
-                </div>
-
-                <span className={styles.available}>{+availableTokensToMint}/20 available</span>
-            </div>
-        </PopupLayout>
-    )
+    // const [amount, setAmount] = useState(1)
+    // const {
+    //     isViewActiveButton,
+    //     availableTokensToMint,
+    //     handlerClickActionButton,
+    //     checkIsDisableButton,
+    //     isError,
+    //     isLoading,
+    //     isApprove
+    // } = useMint(amount)
+    //
+    // const handlerCounter = (value: number) => {
+    //     const plannedValue = amount + value
+    //     if (plannedValue > 0 && plannedValue <= +availableTokensToMint) {
+    //         setAmount(prev => prev + value)
+    //     }
+    // }
+    //
+    // return (
+    //     <PopupLayout>
+    //         <div className={styles.popup}>
+    //             <span className={styles.title}>buy NFT with $DC</span>
+    //
+    //             <div className={styles.buttons}>
+    //                 <div className={styles.counter}>
+    //                     <span onClick={() => handlerCounter(-1)} className={styles.minus}>-</span>
+    //                     <span className={styles.value}>{amount}</span>
+    //                     <span onClick={() => handlerCounter(1)} className={styles.plus}>+</span>
+    //                 </div>
+    //
+    //                 <Button
+    //                     onClick={handlerClickActionButton}
+    //                     className={classNames(styles.button, {
+    //                         [styles.disabled]: checkIsDisableButton(),
+    //                         [styles.loading]: isLoading,
+    //                         [styles.error]: isError
+    //                     })}
+    //                 >
+    //                     {
+    //                         isViewActiveButton &&
+    //                         <span className={styles.text}>
+    //                                 {
+    //                                     isApprove
+    //                                         ? 'click to buy'
+    //                                         : 'approve $DC'
+    //                                 }
+    //                             </span>
+    //                     }
+    //
+    //                     {
+    //                         isLoading && <LoadingButton/>
+    //                     }
+    //
+    //                     {
+    //                         isError && <ErrorButton/>
+    //                     }
+    //                 </Button>
+    //             </div>
+    //
+    //             <span className={styles.available}>{+availableTokensToMint}/20 available</span>
+    //         </div>
+    //     </PopupLayout>
+    // )
 }
 
 export default PopupBuyNft
