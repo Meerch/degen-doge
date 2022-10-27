566f2d9648b54d598c392a393e51dd61cdb20cdd
Meerch <khukhriansk333@gmail.com>
2022-10-28 01:10:43 +0300

    add new wallet Petra

---
+++ svg/svgPetra.tsx
@@ -0,0 +1,11 @@
+export const svgPetra = <svg width="32" height="44" viewBox="0 0 32 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clipPath="url(#clip0_862_38)">
+        <path d="M14.6865 43.685C6.75351 43.685 0.32251 37.254 0.32251 29.321V5.5385L14.6865 0.315V43.685Z" fill="#FF5F5F"/>
+        <path d="M17.3135 25.843C25.2465 25.843 31.6775 19.412 31.6775 11.479V5.5385L17.3135 0.315V25.843Z" fill="#FF5F5F"/>
+    </g>
+    <defs>
+        <clipPath id="clip0_862_38">
+            <rect width="32" height="44" fill="white"/>
+        </clipPath>
+    </defs>
+</svg>
